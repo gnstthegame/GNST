98c60ed95384df558abc3b1f2732f160a6429414
Artur Dziubich <dziubich@interia.eu>
2019-05-20 11:59:54 +0200

    poprawione miasto i dom

---
+++ Assets/Battle/TurnOffTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// aktywuje pole walki
+/// </summary>
+public class TurnOffTrigger : MonoBehaviour {
+    public GameObject on, off;
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            off.SetActive(false);
+            off.SetActive(true);
+        }
+    }
+
+}
